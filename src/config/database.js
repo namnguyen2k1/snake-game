23e035683e3c5acd6e82432860a9b4f2a0d32904
namnguyen2k1 <nanam133hg@gmail.com>
2025-09-13 06:40:56 +0700

    chore(db): create .env.example

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,12 @@ import mongoose, { connect } from "mongoose";
 
 export const connectDatabase = async () => {
   try {
-    const dbName = "snake-game";
-    const uri = `mongodb://localhost:27017/${dbName}`;
+    const dbName = process.env.DB_NAME ?? "snake-game";
+    const uri = process.env.DB_URL ?? `mongodb://localhost:27017/${dbName}`;
 
     await connect(uri).then(() => {
       mongoose.set("debug", true);
-      console.log(`MongoDB connected: ${dbName}`);
+      console.log(`MongoDB connected: ${uri}`);
     });
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
